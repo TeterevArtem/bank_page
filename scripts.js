1ad6c136d6ceb0da6394ec2eff5ceef9dabd9140
TeterevArtem <teterev.artem@gmail.com>
2020-01-22 12:25:02 +0600

    middle complete of bank pages

--- scripts.js
+++ scripts.js
@@ -1,13 +1,21 @@
 $(document).ready(function(){
-    $('.bank__link').on('click', function(e){
-        e.preventDefault();
-        var $this = $(this),
-            content = $this.closest('.wrapper').find('.content__item'),
-            active = content.filter('.active'),
-            href = $this.attr('href');
+  $('.bank__link').on('click', function(e){
+    e.preventDefault();
 
+    var $this = $(this),
+        item = $this.closest('.bank__item'),
+        container = $this.closest('.wrapper'),
+        contentItems = container.find('.content__item'),
+        active = contentItems.filter('.active'),
+        href = $this.attr('href'),
+        reqItem = contentItems.filter('.content__item_' + href),
+        duration = 400;
+    
+      item.addClass('active').siblings().removeClass('active');
+      active.stop(true).fadeOut(duration, function(){
+        reqItem.stop(true).fadeIn(duration).addClass('active').siblings().removeClass('active')
+      })
+    
 
-
-
-    })
+  })
 });
