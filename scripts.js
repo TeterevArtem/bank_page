3bff972ffab386a7cf3f06dd09cdca35584f2f0e
TeterevArtem <teterev.artem@gmail.com>
2020-01-30 17:55:52 +0600

    bank__pages__commplete

--- scripts.js
+++ scripts.js
@@ -15,7 +15,38 @@ $(document).ready(function(){
       active.stop(true).fadeOut(duration, function(){
         reqItem.stop(true).fadeIn(duration).addClass('active').siblings().removeClass('active')
       })
-    
-
   })
 });
+
+
+(function(){
+  $('.review__button').on('click', function(){
+    var $this = $(this),
+        wrapper = $this.closest('.wrapper'),
+        cover = wrapper.closest('body').find('.popup__full_cover');
+    cover.fadeIn()
+    wrapper.addClass('blur')
+  })
+  $('.popup__close').on('click', function(e){
+    e.preventDefault();
+    var $this = $(this),
+        cover = $this.closest('.popup__full_cover'),
+        wrapper = cover.closest('body').find('.wrapper')
+    cover.fadeOut();
+    wrapper.removeClass('blur')
+  })
+})()
+
+$(document).ready(function(){
+  $('.question__show').on('click', function(e){
+    e.preventDefault();
+    var list = $('.question__list');
+    if(list.hasClass('hidden')) {
+      list.removeClass('hidden')
+    }else{
+      list.addClass('hidden')
+    }
+
+  })
+})
+
